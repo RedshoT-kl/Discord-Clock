a3659e4773e08c9a296910601de6d835d2fb4a95
RedshoT-kl <66035993+RedshoT-kl@users.noreply.github.com>
2020-09-18 13:15:22 +0430

    Create Config.js

---
+++ Files/Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: '', //Set Your Bot Token
+    clockchannel: '', //Set Your Channel ID
+    timezone: 'Asia/Tehran', //Set Your Time Zone
+    format: 'HH:mm', //Set Your Favorite Form => https://momentjs.com/docs/#/displaying/format/
+    updateinterval: 600000, // Discord is ratelimiting us for 10 minutes!
+  }
+  //-----------------------------🌿 RedshoT 🌿-------------------------------//
