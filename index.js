60ca38491bed65e0d29f75e52bc287b6146d510e
RedshoT-kl <66035993+RedshoT-kl@users.noreply.github.com>
2020-09-18 13:14:36 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//-----------------------------🌿 RedshoT 🌿-------------------------------//
+//Import Packages
+
+const moment = require('moment');
+const tz = require('moment-timezone');
+const chalk = require('chalk');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+//-----------------------------🌿 RedshoT 🌿-------------------------------//
+//Congig.js Setting 
+
+const { timezone, format, clockchannel, updateinterval, token} = require('./config.js');
+//-----------------------------🌿 RedshoT 🌿-------------------------------//
+//Events 
+
+
+client.once('ready', () => {
+
+  const timeNow = moment().tz(timezone).format(format);
+
+  const clockChannel = client.channels.cache.get(clockchannel);
+
+  clockChannel.edit({ name: `╠  「🕒」ᴛɪᴍᴇ: ${timeNow}` }, 'Clock update')
+    .catch(console.error);
+
+  setInterval(() => {
+    const timeNowUpdate = moment().tz(timezone).format(format);
+    clockChannel.edit({ name: `╠  「🕒」ᴛɪᴍᴇ: ${timeNowUpdate}` }, 'Clock update')
+      .catch(console.error);
+  }, updateinterval);
+
+	console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}\n DeVeLoPeD By RedshoT`);
+});
+//-----------------------------🌿 RedshoT 🌿-------------------------------//
+//Bot Login
+client.login(token);
